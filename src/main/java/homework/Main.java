e55fa4e4f7d36a7bf1f0f63401138c586ef14a42
Abriel <abrielz11@gmail.com>
2025-01-21 14:27:40 +0300

    coordinator doen

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -2,18 +2,16 @@ package homework;
 
 import homework.model.Request;
 import homework.service.Coordinator;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
-
 import static homework.util.Util.getFilesList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        List<File> beginFiles = getFilesList(Paths.get("src/main/resources/files"), ".txt");
+        List<File> beginFiles = getFilesList(Paths.get("src/main/resources/input"), ".txt");
         Coordinator coordinator = new Coordinator(beginFiles, 2);
 
         List<Request> taskRequests = coordinator.mapToWorkerRequest(beginFiles, 2);
